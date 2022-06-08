7bf2bff621e84f4e8114fbeede6f2a3d940d04f3
Hanan-Hussein <hanhuss035@gmail.com>
2022-06-08 22:31:59 +0300

    ft:profile-added a profile model

--- logic/models.py
+++ logic/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    profilephoto = CloudinaryField("profilephoto")
+    bio = models.TextField()
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return f"Profile: {self.username} : email: {self.email} : profile: {self.profilephoto} : category: {self.bio}"
+
+
+    def __str__(self):
+        return self.user.username
+
+    @classmethod
+    def save_profile(cls, profile):
+        cls.save(profile)
+
+    @classmethod
+    def update_profile(cls, username,email,bio,profilephoto):
+        cls.update(username=username, email=email, bio=bio, profilephoto=profilephoto)
+
+    @classmethod
+    def delete_profile(cls, profile):
+        cls.delete(profile)
+

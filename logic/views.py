4166abae9221b8590d7ff21a49337d205e6927c1
Hanan-Hussein <hanhuss035@gmail.com>
2022-06-09 07:41:24 +0300

    ft:register-added a register request functionality

--- logic/views.py
+++ logic/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import NewUserForm, LoginForm, ImageForm, ProfileEditForm,CommentsForm
+# Create your views here.
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Image, Profile, Followers, Like, Comments
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful, Please Login")
+            return redirect("login")
+        messages.error(
+            request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request, template_name="auth/register.html", context={"register_form": form})

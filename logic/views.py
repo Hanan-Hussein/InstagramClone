60e856fcbcf17504101ab1f70d7848b32c49e68a
Hanan-Hussein <hanhuss035@gmail.com>
2022-06-09 07:43:35 +0300

    ft:login request-created login request functionality

--- logic/views.py
+++ logic/views.py
@@ -22,3 +22,21 @@ def register_request(request):
             request, "Unsuccessful registration. Invalid information.")
     form = NewUserForm()
     return render(request, template_name="auth/register.html", context={"register_form": form})
+
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect('login')
+    form = LoginForm()
+    context = {
+        "form": form,
+    }
+    return render(request, 'auth/login.html', context=context)
+
